5e25643cbb340d3d4b3b64bfbc7050c7f6cffd9c
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-01-16 08:10:19 -0300

    Save

--- Assets/Scripts/Ship/ShipLandingController.cs
+++ Assets/Scripts/Ship/ShipLandingController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipLandingController : MonoBehaviour
@@ -15,16 +17,17 @@ public class ShipLandingController : MonoBehaviour
 
     #endregion
 
-    #region Getters && Setters
-
-    public bool IsWantedDeployed { get { return _isWantedDeployed; } set { _isWantedDeployed = value; } }
-
-    #endregion
-
     #region Private Variables
 
-    private bool _isWantedDeployed = false;
-    private bool _isLandingGearDeployed = false;
+    private enum LandingGearState
+    {
+        Up,
+        Between,
+        Down
+    }
+    private LandingGearState _isWantedDeployed = LandingGearState.Down;
+    private LandingGearState _isLandingGearDeployed = LandingGearState.Down;
+    private LandingGearState _previousState = LandingGearState.Down;
 
     #endregion
 
@@ -33,21 +36,38 @@ public class ShipLandingController : MonoBehaviour
     private void Start()
     {
         PlayerInputHandler.OnSpecialAction += HandleSpecialAction;
-
-        EnableLandingGearColliders(_isWantedDeployed);
     }
 
     private void FixedUpdate()
     {
-        if (_isWantedDeployed == _isLandingGearDeployed) { return; }
+        float wantedYPosition = 0;
 
-        float wantedYPosition = _isLandingGearDeployed == true ? _landingGearStoredYPos : _landingGearDeployedYPos;
+        if (_isWantedDeployed == LandingGearState.Down)
+        {
+            wantedYPosition = _landingGearStoredYPos;
+        }
+        else if (_isWantedDeployed == LandingGearState.Up)
+        {
+            wantedYPosition = _landingGearDeployedYPos;
+        }
+        else
+        {
+            wantedYPosition = _landingGearTransform.localPosition.y;
+        }
 
         _landingGearTransform.localPosition = new
             Vector3(_landingGearTransform.localPosition.x,
             Mathf.Lerp(_landingGearTransform.localPosition.y, wantedYPosition, Time.deltaTime), _landingGearTransform.localPosition.z);
 
-        if (Mathf.RoundToInt(_landingGearTransform.localPosition.y) == Mathf.RoundToInt(wantedYPosition)) { _isLandingGearDeployed = _isWantedDeployed; }
+        if (_isWantedDeployed != LandingGearState.Between)
+        {
+            if (_landingGearTransform.localPosition.y <= _landingGearDeployedYPos + 0.3f && _isWantedDeployed == LandingGearState.Up) { _landingGearTransform.localPosition = new Vector3(_landingGearTransform.localPosition.x, _landingGearDeployedYPos, _landingGearTransform.localPosition.z); _isLandingGearDeployed = LandingGearState.Up; }
+            else if (_landingGearTransform.localPosition.y >= _landingGearStoredYPos - 0.3f && _isWantedDeployed == LandingGearState.Down) { _landingGearTransform.localPosition = new Vector3(_landingGearTransform.localPosition.x, _landingGearStoredYPos, _landingGearTransform.localPosition.z); _isLandingGearDeployed = LandingGearState.Down; }
+        }
+        else
+        {
+            _isLandingGearDeployed = LandingGearState.Between;
+        }
     }
 
     private void OnDestroy()
@@ -57,26 +77,23 @@ public class ShipLandingController : MonoBehaviour
 
     #endregion
 
-    private void EnableLandingGearColliders(bool isEnabled)
-    {
-        foreach (Collider collider in _landingGearColliders)
-        {
-            collider.isTrigger = !isEnabled;
-        }
-    }
-
     private void HandleSpecialAction(PlayerInputHandler player, bool isPressed)
     {
-        if(_isWantedDeployed != _isLandingGearDeployed) { return; }
-
         PlayerInteractionController playerInteractionController = _booster.CurrentPlayer as PlayerInteractionController;
 
         //If player that pressed action button is not on booster, return
         if (player != playerInteractionController?.PlayerInput) { return; }
 
-        if (!isPressed) { return; }
+        if (_isWantedDeployed == _isLandingGearDeployed && _isWantedDeployed != LandingGearState.Between) { _previousState = _isWantedDeployed; _isWantedDeployed = ReverseState(_isWantedDeployed); }
+        else if (_isWantedDeployed != _isLandingGearDeployed) { _previousState = _isWantedDeployed; _isWantedDeployed = LandingGearState.Between; }
+        else if (_isWantedDeployed == _isLandingGearDeployed && _isWantedDeployed == LandingGearState.Between) { _isWantedDeployed = ReverseState(_previousState); }
+    }
 
-        _isWantedDeployed = !_isWantedDeployed;
-        EnableLandingGearColliders(_isWantedDeployed);
+    private LandingGearState ReverseState(LandingGearState state)
+    {
+        if (state == LandingGearState.Up) { state = LandingGearState.Down; }
+        else if (state == LandingGearState.Down) { state = LandingGearState.Up; }
+
+        return state;
     }
 }

bd5c4be5393a2678db82d1cd3089e2b9f2db2a5e
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-01-17 10:03:30 -0300

    Save

--- Assets/Scripts/Combat/ConstantLaser.cs
+++ Assets/Scripts/Combat/ConstantLaser.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConstantLaser : MonoBehaviour
+public class ConstantLaser : WeaponShoot
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Private Variables
+
+    private bool _isShooting = false;
+    private GameObject _laserBeam;
+
+    #endregion
+
+    public override void CheckShootInput()
     {
-        
+        if (_weapon.CurrentPlayer.IsUsing)
+        {
+            Shoot();
+        }
+        else 
+        {
+            _laserBeam.transform.SetParent(null);
+            Destroy(_laserBeam);
+            _isShooting = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Shoot()
     {
-        
+        if (!_isShooting) 
+        { 
+            _laserBeam = Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransforms[0].position, _weapon.TurretHead.rotation);
+            _laserBeam.transform.SetParent(_weapon.TurretHead);
+            _isShooting = true;
+        }
+        else { return; }
     }
+
 }

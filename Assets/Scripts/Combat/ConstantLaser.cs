53f1572fa74c53f6989142fc058b0b6e6b3d4e55
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-01-31 09:59:50 -0300

    Save

--- Assets/Scripts/Combat/ConstantLaser.cs
+++ Assets/Scripts/Combat/ConstantLaser.cs
@@ -6,8 +6,11 @@ public class ConstantLaser : WeaponShoot
 {
     #region Private Variables
 
+    private GameObject _constantLaser;
     private GameObject _laserBeam;
+    private GameObject _laserBall;
     private ShootLaserState _shootLaserState = ShootLaserState.CanShoot;
+    [SerializeField] private float _laserBallScaleTime = 0;
 
     #endregion
 
@@ -17,48 +20,73 @@ public class ConstantLaser : WeaponShoot
     {
         base.Start();
 
-        _laserBeam = Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransforms[0].position, _weapon.TurretHead.rotation);
+        _constantLaser = Instantiate(_weapon.ProjectilePrefab, _weapon.ShootTransforms[0].position, _weapon.TurretHead.rotation);
+        _laserBeam = _constantLaser.transform.GetChild(0).gameObject;
+        _laserBall = _constantLaser.transform.GetChild(1).gameObject;
 
-        _laserBeam.transform.GetChild(0).gameObject.SetActive(false);
+        _laserBeam.SetActive(false);
+        _laserBall.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (_shootLaserState == ShootLaserState.ChargingUp && _laserBall.transform.localScale != new Vector3(1.5f, 1.5f, 1.5f))
+        {
+            _laserBallScaleTime = _laserBallScaleTime + Time.deltaTime;
+            _laserBall.transform.localScale = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(1.5f, 1.5f, 1.5f), _laserBallScaleTime);
+        }
+        if (_shootLaserState == ShootLaserState.ChargingDown && _laserBall.transform.localScale != new Vector3(0, 0, 0))
+        {
+            _laserBallScaleTime = _laserBallScaleTime - Time.deltaTime;
+            _laserBall.transform.localScale = Vector3.Lerp(new Vector3(0, 0, 0), new Vector3(1.5f, 1.5f, 1.5f), _laserBallScaleTime);
+        }
     }
 
     #endregion
 
     public override void CheckShootInput()
     {
-        _laserBeam.transform.rotation = _weapon.TurretHead.transform.rotation;
-        _laserBeam.transform.position = _weapon.TurretHead.transform.position;
+        _constantLaser.transform.rotation = _weapon.TurretHead.transform.rotation;
+        _constantLaser.transform.position = _weapon.TurretHead.transform.position;
 
-        if (_weapon.CurrentPlayer.IsUsing && !_laserBeam.transform.GetChild(0).gameObject.activeSelf && _shootLaserState == ShootLaserState.CanShoot)
+        if (_weapon.CurrentPlayer.IsUsing && !_laserBeam.activeSelf && (_shootLaserState == ShootLaserState.CanShoot || _shootLaserState == ShootLaserState.ChargingUp))
         {
-            StartCoroutine(ShootLaserBeam());
+            ShootLaserBeam();
         }
-        else if (!_weapon.CurrentPlayer.IsUsing && _laserBeam.transform.GetChild(0).gameObject.activeSelf && _shootLaserState == ShootLaserState.CanStop)
+        else if (!_weapon.CurrentPlayer.IsUsing && _laserBeam.activeSelf && (_shootLaserState == ShootLaserState.CanStop || _shootLaserState == ShootLaserState.ChargingDown))
         {
-            StartCoroutine(StopLaserBeam());
+            StopLaserBeam();
         }
     }
 
-    IEnumerator ShootLaserBeam()
+    private void ShootLaserBeam()
     {
-        _shootLaserState = ShootLaserState.WaitingPhase;
-        yield return new WaitForSeconds(1);
-        _laserBeam.transform.GetChild(0).gameObject.SetActive(true);
-        _shootLaserState = ShootLaserState.CanStop;
+        _shootLaserState = ShootLaserState.ChargingUp;
+        if (_laserBallScaleTime >= 1)
+        {
+            _laserBeam.SetActive(true);
+            _shootLaserState = ShootLaserState.CanStop;
+        }
     }
 
-    IEnumerator StopLaserBeam()
+    private void StopLaserBeam()
     {
-        _shootLaserState = ShootLaserState.WaitingPhase;
-        _laserBeam.transform.GetChild(0).gameObject.SetActive(false);
-        yield return new WaitForSeconds(1);
-        _shootLaserState = ShootLaserState.CanShoot;
+        _shootLaserState = ShootLaserState.ChargingDown;
+        _laserBeam.SetActive(false);
+
+        if (_laserBall.transform.localScale == new Vector3(0, 0, 0))
+        {
+            _shootLaserState = ShootLaserState.CanShoot;
+        }
     }
 
     private enum ShootLaserState
     {
         CanShoot,
-        WaitingPhase,
+        ChargingUp,
+        ChargingDown,
         CanStop
     }
 }

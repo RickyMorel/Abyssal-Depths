b3dfa89e26f7d4bb18da309462caaad6e1575fa2
RickyMorel <ricky@attackwagon.com>
2023-02-01 08:35:26 -0300

    Created Item Ids

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,10 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+public class ScriptableObjectIdAttribute : PropertyAttribute { }
+
+//Generates new id
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(ScriptableObjectIdAttribute))]
+public class ScriptableObjectIdDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        if (string.IsNullOrEmpty(property.stringValue))
+        {
+            property.stringValue = Guid.NewGuid().ToString();
+        }
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endif
 
 [CreateAssetMenu(fileName = "Data", menuName = "Inventory/Item", order = 1)]
 public class Item : ScriptableObject
 {
+    [ScriptableObjectId]
+    public string Id;
     public Sprite Icon;
     public string DisplayName;
     public string Description;

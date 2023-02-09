1c241cac2a9bc6cdb52329ffd7190c8231e408dc
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-02-09 11:15:15 -0300

    Save

--- Assets/Scripts/Combat/ChipDataSO.cs
+++ Assets/Scripts/Combat/ChipDataSO.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+#endif
+
 [CreateAssetMenu(fileName = "Data", menuName = "Combat/Chip")]
 
 public class ChipDataSO : ScriptableObject
 {
     public List<ChipData> ChipData = new List<ChipData>();
+
+    #region Editor
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(ChipDataSO))]
+    public class ChipDataEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+
+#endif
+    #endregion
 }
 [System.Serializable]
 public class ChipData

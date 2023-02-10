b98fe444bea64070cd06f0e4910e2fed2b8791c9
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-02-10 09:06:54 -0300

    Save

--- Assets/Scripts/Combat/ChipDataSO.cs
+++ Assets/Scripts/Combat/ChipDataSO.cs
@@ -2,40 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-
-using UnityEditor;
-
-#endif
-
 [CreateAssetMenu(fileName = "Data", menuName = "Combat/Chip")]
 
 public class ChipDataSO : ScriptableObject
 {
-    public List<ChipData> ChipData = new List<ChipData>();
-
-    #region Editor
-#if UNITY_EDITOR
-
-    [CustomEditor(typeof(ChipDataSO))]
-    public class ChipDataEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-        }
-    }
-
-#endif
-    #endregion
+    public BaseChip BaseData;
+    public FireChip FireData;
+    public ElectricChip ElectricData;
+    public LaserChip LaserData;
 }
+
 [System.Serializable]
-public class ChipData
+public class BaseChip
 {
-    public ChipType Chiptype;
-    public int ChipLevel;
     public float ShootAfterSeconds;
-    public int Damage;
+    public int MinimumDamage;
+    public int MaximumDamage;
+    public int MinimumDamageMK2;
+    public int MaximumDamageMK2;
+    public int MinimumDamageMK3;
+    public int MaximumDamageMK3;
+    public int DamageMultiplierWeakness;
+    public int DamageMultiplierResistance;
+}
+
+[System.Serializable]
+public class FireChip
+{
+    public float ShootAfterSeconds;
+    public float HitDamage;
+    public int AfterburnDamage;
+    public int AfterburnDamageMK2;
+    public int AfterburnDamageMK3;
+    public float BurnAfterSeconds;
+    public float BurnDuration;
+    public int DamageMultiplierWeakness;
+    public int DamageMultiplierResistance;
+}
+
+[System.Serializable]
+public class ElectricChip
+{
+    public float ShootAfterSeconds;
+    public float HitDamage;
+    public int AfterburnDamage;
+    public int AfterburnDamageMK2;
+    public int AfterburnDamageMK3;
+    public int DamageMultiplierWeakness;
+    public int DamageMultiplierResistance;
+}
+
+[System.Serializable]
+public class LaserChip
+{
+    public float ShootAfterSeconds;
+    public float HitDamage;
+    public int AfterburnDamage;
+    public int AfterburnDamageMK2;
+    public int AfterburnDamageMK3;
     public int DamageMultiplierWeakness;
     public int DamageMultiplierResistance;
 }

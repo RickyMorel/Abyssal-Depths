b4b5c0062b71da928fc4ee72e5061373b19cea98
RickyMorel <98762220+RickyMorel@users.noreply.github.com>
2023-03-01 09:06:38 -0300

    Blocking Plane Code

--- Assets/Scripts/EditorTools/WalkPlaneVisual.cs
+++ Assets/Scripts/EditorTools/WalkPlaneVisual.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This is just a class find the walk plane instance
+[ExecuteInEditMode]
 public class WalkPlaneVisual : MonoBehaviour
 {
     #region Private Variables
@@ -19,6 +20,8 @@ public class WalkPlaneVisual : MonoBehaviour
 
     private void Awake()
     {
+        if (!Application.isPlaying) { return; }
+
         if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
@@ -27,6 +30,37 @@ public class WalkPlaneVisual : MonoBehaviour
         {
             _instance = this;
         }
+    }
+
+    private void OnEnable()
+    {
+        if (!Application.isEditor || Application.isPlaying) { return; }
+
+        CreateNavMeshBlockerGOs();
+    }
+
+    private void CreateNavMeshBlockerGOs()
+    {
+        Transform blockingPlane_1 = transform.Find("BlockingPlane_1");
+
+        if (blockingPlane_1 != null)
+        {
+            //blockingPlane_1 = blockingPlane_1.gameObject;
+            //blockingPlane_1.transform.position = _enemyBot.Path[0].Position;
+            return;
+        }
+
+        GameObject newBlockingPlane_1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        newBlockingPlane_1.name = "BlockingPlane_1";
+        newBlockingPlane_1.transform.parent = transform;
+        newBlockingPlane_1.transform.localScale = new Vector3(1f, 500f, 100f);
+        newBlockingPlane_1.isStatic = true;
+        newBlockingPlane_1.GetComponent<Renderer>().enabled = false;
+        DestroyImmediate(newBlockingPlane_1.GetComponent<Collider>());
+
+        Vector3 spawnPos = new Vector3(0f, 0f, newBlockingPlane_1.transform.localScale.z / 2f + 0.5f);
+
+        newBlockingPlane_1.transform.localPosition = spawnPos;
     }
 
     public bool IsWithinBounds(Vector3 pos)

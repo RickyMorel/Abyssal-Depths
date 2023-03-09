aafaa1ad7097f1182df4535a3d3e1a23a06d6f23
RickyMorel <ricardomorel0903@gmail.com>
2023-03-09 08:53:04 -0300

    Connected Buttons

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    #region Editor Fields
+
+    [SerializeField] private GameObject _headerPanel;
+    [SerializeField] private GameObject _playPanel;
+    [SerializeField] private GameObject _settingsPanel;
+    [SerializeField] private GameObject _loadPanel;
+    [SerializeField] private GameObject _gameplaySettingsPanel;
+    [SerializeField] private GameObject _videoSettingsPanel;
+    [SerializeField] private GameObject _audioSettingsPanel;
+
+    #endregion
 
     public void NewGame()
     {
@@ -14,5 +25,53 @@ public class MainMenuUI : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void DisableAllPanels()
+    {
+        _headerPanel.SetActive(false);
+        _playPanel.SetActive(false);
+        _settingsPanel.SetActive(false);
+        _loadPanel.SetActive(false);
+        _gameplaySettingsPanel.SetActive(false);
+        _videoSettingsPanel.SetActive(false);
+        _audioSettingsPanel.SetActive(false);
+    }
+
+    public void SelectPlayPanel()
+    {
+        DisableAllPanels();
+
+        _playPanel.SetActive(true);
+        _headerPanel.SetActive(true);
+    }
+
+    public void SelectSettingsPanel()
+    {
+        DisableAllPanels();
+
+        _settingsPanel.SetActive(true);
+        _headerPanel.SetActive(true);
+    }
+
+    public void SelectGameplayPanel()
+    {
+        DisableAllPanels();
+
+        _gameplaySettingsPanel.SetActive(true);
+    }
+
+    public void SelectVideoPanel()
+    {
+        DisableAllPanels();
+
+        _videoSettingsPanel.SetActive(true);
+    }
+
+    public void SelectAudioPanel()
+    {
+        DisableAllPanels();
+
+        _audioSettingsPanel.SetActive(true);
     }
 }

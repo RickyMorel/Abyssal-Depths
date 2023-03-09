232a63a387adaf534e28567843ba372f2879a3a5
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-03-09 15:14:57 -0300

    Fixed mace better

--- Assets/Scripts/Player/AttackHitBox.cs
+++ Assets/Scripts/Player/AttackHitBox.cs
@@ -89,10 +89,23 @@ public class AttackHitBox : MonoBehaviour
 
     private void CalculateDamage(Damageable health)
     {
-        _damage = _damageData.Damage[0];
+        _damage = (int)_damageData.SecondaryValue[0];
         health.DamageData = _damageData;
-
-        if (_mace.rb.velocity.x <= _mace.MaxMovementSpeed / 2 && _mace.rb.velocity.y < _mace.MaxMovementSpeed / 2) { _damage = _damage / 2; health.Damage(_damage); }
-        else { health.Damage(_damage); }
+        if (Mathf.Abs(_mace.rb.velocity.x) >= (_mace.MaxMovementSpeed / 2) || Mathf.Abs(_mace.rb.velocity.y) >= (_mace.MaxMovementSpeed / 2)) 
+        {
+            float damagePercentage;
+            if (Mathf.Abs(_mace.rb.velocity.x) >= Mathf.Abs(_mace.rb.velocity.y))
+            {
+                damagePercentage = (Mathf.Abs(_mace.rb.velocity.x) * 100) / _mace.MaxMovementSpeed;
+            }
+            else
+            {
+                damagePercentage = (Mathf.Abs(_mace.rb.velocity.y) * 100) / _mace.MaxMovementSpeed;
+            }
+            _damage = (int)((_damage * damagePercentage) / 100);
+            Debug.Log(_damage);
+            health.Damage(_damage); 
+        }
+        else { health.Damage(_damageData.Damage[0]); }
     }
 }

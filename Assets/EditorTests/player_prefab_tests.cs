be7643f4cc6d2507369ad9c6feaa5a2f319ff54c
RickyMorel <ricardomorel0903@gmail.com>
2023-03-22 09:21:08 -0300

    PlayerPrefab Checks

--- Assets/EditorTests/player_prefab_tests.cs
+++ Assets/EditorTests/player_prefab_tests.cs
@@ -29,6 +29,8 @@ namespace Tests
     {
         private readonly GameObject _playerRootPrefab;
         private readonly GameObject _playerPrefab;
+        private readonly string PlayerLayer = "Player";
+        private readonly string EnemyHitBoxLayer = "EnemyHitBox";
 
         public player_prefab_tests(GameObject playerRootPrefab)
         {
@@ -87,7 +89,7 @@ namespace Tests
         [Test]
         public void check_if_player_is_on_player_layer()
         {
-            Assert.IsTrue(_playerPrefab.layer == LayerMask.NameToLayer("Player"), "Player object isn't on correct layer");
+            Assert.IsTrue(_playerPrefab.layer == LayerMask.NameToLayer(PlayerLayer), "Player object isn't on correct layer");
         }
 
         [Test]
@@ -95,6 +97,91 @@ namespace Tests
         {
             Assert.IsTrue(_playerPrefab.tag == "Player", "Player object doesn't have correct tag");
         }
+
+        #region AttackHitBox Tests
+
+        [Test]
+        public void check_if_player_attackHitBox_is_on_correct_layer()
+        {
+            GameObject hitBox = _playerPrefab.GetComponentInChildren<AttackHitBox>().gameObject;
+            bool isCorrectLayer = hitBox.layer == LayerMask.NameToLayer(EnemyHitBoxLayer);
+
+            Assert.IsTrue(isCorrectLayer, "Player attackHitBox is on incorrect layer");
+        }
+
+        [Test]
+        public void check_if_player_attackHitBox_has_collider_disabled()
+        {
+            GameObject hitBox = _playerPrefab.GetComponentInChildren<AttackHitBox>().gameObject;
+            
+            Assert.IsTrue(hitBox.GetComponent<Collider>().enabled == false, "Player attackHitBox collider is enabled on start");
+        }
+
+        [Test]
+        public void check_if_player_attackHitBox_collider_is_trigger()
+        {
+            GameObject hitBox = _playerPrefab.GetComponentInChildren<AttackHitBox>().gameObject;
+
+            Assert.IsTrue(hitBox.GetComponent<Collider>().isTrigger == true, "Player attackHitBox collider is not trigger");
+        }
+
+        [Test]
+        public void check_if_player_attackHitBox_rigidbody_is_kinematic()
+        {
+            GameObject hitBox = _playerPrefab.GetComponentInChildren<AttackHitBox>().gameObject;
+            bool isKinematic = hitBox.GetComponent<Rigidbody>().isKinematic == true;
+
+            Assert.IsTrue(isKinematic, "Player attackHitBox is not kinematic");
+        }
+
+        #endregion
+
+        #region GroundCheck Tests
+
+        [Test]
+        public void check_if_player_groundCheck_is_on_correct_layer()
+        {
+            GameObject groundCheckObj = _playerPrefab.GetComponentInChildren<PlayerGroundCheck>().gameObject;
+            bool isCorrectLayer = groundCheckObj.layer == LayerMask.NameToLayer(PlayerLayer);
+
+            Assert.IsTrue(isCorrectLayer, "GroundCheck is on incorrect layer");
+        }
+
+        [Test]
+        public void check_if_player_groundCheck_has_collider_enabled()
+        {
+            GameObject groundCheckObj = _playerPrefab.GetComponentInChildren<PlayerGroundCheck>().gameObject;
+
+            Assert.IsTrue(groundCheckObj.GetComponent<Collider>().enabled == true, "GroundCheck collider is disabled on start");
+        }
+
+        [Test]
+        public void check_if_player_groundCheck_collider_is_trigger()
+        {
+            GameObject groundCheckObj = _playerPrefab.GetComponentInChildren<PlayerGroundCheck>().gameObject;
+
+            Assert.IsTrue(groundCheckObj.GetComponent<Collider>().isTrigger == true, "GroundCheck collider is not trigger");
+        }
+
+        [Test]
+        public void check_if_player_groundCheck_rigidbody_is_kinematic()
+        {
+            GameObject groundCheckObj = _playerPrefab.GetComponentInChildren<PlayerGroundCheck>().gameObject;
+            bool isKinematic = groundCheckObj.GetComponent<Rigidbody>().isKinematic == true;
+
+            Assert.IsTrue(isKinematic, "GroundCheck is not kinematic");
+        }
+
+        [Test]
+        public void check_if_player_groundCheck_has_correct_layers()
+        {
+            PlayerGroundCheck groundCheck = _playerPrefab.GetComponentInChildren<PlayerGroundCheck>();
+
+            Assert.IsTrue(groundCheck.FloorLayers == (groundCheck.FloorLayers | (1 << LayerMask.NameToLayer("Floor"))), "GroundCheck doesn't contain Floor layer");
+            Assert.IsTrue(groundCheck.FloorLayers == (groundCheck.FloorLayers | (1 << LayerMask.NameToLayer("ShipFloor"))), "GroundCheck doesn't contain ShipFloor layer");
+        }
+
+        #endregion
 
         #endregion
     }

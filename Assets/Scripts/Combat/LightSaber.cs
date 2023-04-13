87b3f2a02427265608d994d57b391b9e06f87f7c
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-04-13 08:59:55 -0400

    tried to add boomerang effect to lightsaber

--- Assets/Scripts/Combat/LightSaber.cs
+++ Assets/Scripts/Combat/LightSaber.cs
@@ -6,15 +6,15 @@ public class LightSaber : MeleeWeapon
 {
     #region Editor Fields
 
-    [SerializeField] private Transform _handleTransform;
+    [SerializeField] private GameObject _handle;
     [SerializeField] private BoxCollider _bladeBoxCollider;
 
     #endregion
 
     #region Private Variables
 
+    private float _rotationSpeed = 20;
     private bool _isBladeOut = false;
-
 
     #endregion
 
@@ -32,7 +32,7 @@ public class LightSaber : MeleeWeapon
         if (_weapon.CurrentPlayer != null && !_isBladeOut) 
         {
             _isBladeOut = true;
-            TimelinesManager.Instance.LightSaberBlade.gameObject.transform.SetParent(_handleTransform);
+            TimelinesManager.Instance.LightSaberBlade.gameObject.transform.SetParent(_handle.transform);
             TimelinesManager.Instance.LightSaberBlade.gameObject.transform.localPosition = new Vector3(0, 0, 0);
             TimelinesManager.Instance.LightSaberOut.Play();
             _bladeBoxCollider.enabled = true;
@@ -47,4 +47,17 @@ public class LightSaber : MeleeWeapon
     }
 
     #endregion
+
+    public override void Shoot()
+    {
+        _handle.transform.Rotate(_rotationSpeed * Time.deltaTime, 0, 0);
+    }
+
+    public override void CheckShootInput()
+    {
+        if (_weapon.CurrentPlayer.IsUsing)
+        {
+            Shoot();
+        }
+    }
 }

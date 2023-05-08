91d090592609d9439de059b41d7c7d68edb8fd05
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-05-08 08:41:41 -0400

    Fixed an issue with weaponattackhitbox

---
+++ Assets/Scripts/Combat/WeaponAttackHitBox.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttackHitBox : AttackHitBox
+{
+    #region Editors Field
+
+    [SerializeField] protected Weapon _weapon;
+    [SerializeField] private MeleeWeapon _meleeWeapon;
+
+    #endregion
+
+    #region Unity Loops
+
+    public override void Start()
+    {
+        _damageData = DamageData.GetDamageData(_damageTypes, _weapon, -1);
+    }
+
+    #endregion
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6) { InvokeHitParticles(other); }
+
+        if (!other.gameObject.TryGetComponent(out Damageable enemyHealth)) { return; }
+
+        if (_ownHealth != null && enemyHealth == _ownHealth) { return; }
+
+        InvokeHitParticles(other);
+
+        if (enemyHealth is AIHealth)
+        {
+            DealDamageToEnemies((AIHealth)enemyHealth, (_damageData.Damage[0]), (_damageData.Damage[1]));
+        }
+    }
+}

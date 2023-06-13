9bed0fceb06536c4712f4f76e054f224f5c731f4
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-06-13 10:16:13 -0400

    Save

---
+++ Assets/Scripts/NextLevelPortal.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelPortal : MonoBehaviour
+{
+    //1 esta cerrado
+    //2 el jugador interactua con la puerta y esta vibra
+    //3 la puerta empieza a hacer algo y vienen enemigos por 30 segundos
+    //4 la puerta se abre
+}

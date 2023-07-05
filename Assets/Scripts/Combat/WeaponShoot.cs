d7293a35b29d5333f053989ee3605198f1c2ae66
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-07-05 19:58:59 -0400

    Save

--- Assets/Scripts/Combat/WeaponShoot.cs
+++ Assets/Scripts/Combat/WeaponShoot.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class WeaponShoot : MonoBehaviour
 {
     #region Editor Fields
 
+    [Header("Stats")]
     [SerializeField] protected float _recoilForce = 2.5f;
     [SerializeField] protected float _timeBetweenShots = 0.2f;
+    
+    [Header("Sounds")]
+    [SerializeField] private EventReference _shootingSfx;
 
     #endregion
 
@@ -56,6 +61,7 @@ public class WeaponShoot : MonoBehaviour
         if (_timeBetweenShots > _timeSinceLastShot) { return; }
 
         _timeSinceLastShot = 0f;
+        GameAudioManager.Instance.PlaySound(_shootingSfx, transform.position);
         InstantiateProjectile(_weapon.ShootTransforms[0]);
 
         Ship.Instance.AddForceToShip(-_weapon.TurretHead.transform.forward * _recoilForce, ForceMode.Impulse);

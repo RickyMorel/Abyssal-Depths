621fde812cf9b8aedf3a5ac5d383bb72143ec437
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-08-11 19:06:18 -0400

    Save

---
+++ Assets/Scripts/Input/KeyboardScript.cs
@@ -0,0 +1,64 @@
+﻿using Rewired.Integration.UnityUI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyboardScript : MonoBehaviour
+{
+    #region Private Variables
+
+    private RewiredEventSystem _eventSystem;
+
+    #endregion
+
+    #region EditorFields
+
+    [SerializeField] private TMP_InputField _textField;
+    [SerializeField] private GameObject _engLayoutSml, _engLayoutBig, _symbLayout;
+
+    #endregion
+
+    #region Unity Loops
+
+    private void Start()
+    {
+        _eventSystem = GameObject.FindGameObjectWithTag(GameTagsManager.EVENT_SYSTEM).GetComponent<RewiredEventSystem>();
+    }
+
+    #endregion
+
+    public void alphabetFunction(string alphabet)
+    {
+        _textField.text = _textField.text + alphabet;
+    }
+
+    public void BackSpace()
+    {
+        if(_textField.text.Length>0) _textField.text = _textField.text.Remove(_textField.text.Length-1);
+    }
+
+    public void CloseAllLayouts()
+    {
+        _engLayoutSml.SetActive(false);
+        _engLayoutBig.SetActive(false);
+        _symbLayout.SetActive(false);
+    }
+
+    public void ShowLayout(GameObject SetLayout)
+    {
+        CloseAllLayouts();
+        SetLayout.SetActive(true);
+    }
+
+    public void SetGameObjectAsSelected(GameObject gameObjectToBeSelected)
+    {
+        _eventSystem.SetSelectedGameObject(gameObjectToBeSelected);
+    }
+
+    public void Enter()
+    {
+        _eventSystem.SetSelectedGameObject(_textField.gameObject);
+        CloseAllLayouts();
+    }
+}
---
+++ Assets/Scripts/Input/ReselectButtonForInteractables.cs
@@ -0,0 +1,82 @@
+using Rewired;
+using Rewired.Integration.UnityUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReselectButtonForInteractables : ReselectButton
+{
+    #region Editor Fields
+
+    [SerializeField] private Interactable _interactable = null;
+
+    #endregion
+
+    #region Private Variables
+
+    private Player _rewiredPlayer;
+
+    #endregion
+
+    #region Public Properties
+
+    public Interactable Interactable { get { return _interactable; } set { _interactable = value; } }
+    public GameObject LastSelected => _lastSelected;
+    public RewiredEventSystem RewiredEventSystem => _eventSystem;
+
+    #endregion
+
+    #region Unity Loops
+
+    public override void Start()
+    {
+        //do nothing
+    }
+
+    public override void OnDisable()
+    {
+        //do nothing
+    }
+
+    #endregion
+
+    public override IEnumerator LateEnable()
+    {
+        yield return new WaitForEndOfFrame();
+
+        if (_firstButton == null) { _firstButton = GetComponentInChildren<Button>().gameObject; }
+
+        _rewiredPlayer = ReInput.players.GetPlayer(_interactable.CurrentPlayer.PlayerInput.PlayerId);
+        _rewiredPlayer.controllers.maps.SetMapsEnabled(true, "UI");
+        
+        _playerInput = _interactable.CurrentPlayer.PlayerInput;
+
+        _playerInput.OnUIHorizontal += ReselectButtonWhenNeeded;
+        _playerInput.OnUIVertical += ReselectButtonWhenNeeded;
+        _playerInput.OnShoulderLeft += ReselectButtonWhenNeeded;
+        _playerInput.OnShoulderRight += ReselectButtonWhenNeeded;
+        _playerInput.OnUISubmit += ReselectButtonWhenNeeded;
+
+        _playerInput.OnUICancel += GoBackToPreviousScreenOrExit;
+
+        _interactable.Humble.OnUninteract += Unsuscribe;
+
+        _eventSystem = GameObject.FindGameObjectWithTag(GameTagsManager.EVENT_SYSTEM).GetComponent<RewiredEventSystem>();
+    }
+
+    private void Unsuscribe()
+    {
+        _rewiredPlayer.controllers.maps.SetMapsEnabled(false, "UI");
+
+        _playerInput.OnUIHorizontal -= ReselectButtonWhenNeeded;
+        _playerInput.OnUIVertical -= ReselectButtonWhenNeeded;
+        _playerInput.OnShoulderLeft -= ReselectButtonWhenNeeded;
+        _playerInput.OnShoulderRight -= ReselectButtonWhenNeeded;
+        _playerInput.OnUISubmit -= ReselectButtonWhenNeeded;
+
+        _playerInput.OnUICancel -= GoBackToPreviousScreenOrExit;
+
+        _interactable.Humble.OnUninteract -= Unsuscribe;
+    }
+}

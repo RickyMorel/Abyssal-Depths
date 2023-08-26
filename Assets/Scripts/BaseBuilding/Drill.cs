bebfcfc5b833ce862be9a34d67deb94964b41fe9
Arielelnoob115Z <arguelloariel115@gmail.com>
2023-08-26 11:33:35 -0400

    Momentary Save

--- Assets/Scripts/BaseBuilding/Drill.cs
+++ Assets/Scripts/BaseBuilding/Drill.cs
@@ -9,6 +9,13 @@ public class Drill : MonoBehaviour
     [SerializeField] private GameObject _drillHead;
     [SerializeField] private float _drillRotationSpeed;
     [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _timeToReachRotationMaxSpeedMultiplier = 1;
+
+    #endregion
+
+    #region Private Variables
+
+    private float _rotationSpeed;
 
     #endregion
 
@@ -16,23 +23,23 @@ public class Drill : MonoBehaviour
 
     private void Update()
     {
+        if (!DayNightManager.Instance.IsNightTime) { return; }
+        else
+        {
+            if (_rotationSpeed != 0) { _rotationSpeed = 0; }
+        }
+
         DrillRotation();
-    }
 
-    private void OnEnable()
-    {
+        if (_rotationSpeed >= 1) { return; }
 
-    }
-
-    private void OnDisable()
-    {
-        
+        _rotationSpeed += Time.deltaTime * _timeToReachRotationMaxSpeedMultiplier;
     }
 
     #endregion
 
     private void DrillRotation()
     {
-        _drillHead.transform.Rotate(_rotation * Time.deltaTime * _drillRotationSpeed);
+        _drillHead.transform.Rotate(_rotation * Time.deltaTime * _drillRotationSpeed * _rotationSpeed);
     }
 }
